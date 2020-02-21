55cd7ca6bd19bb00f3e1dcf4c933a0fe916ea212
Akash Tripathi <tripk@ap.leaseplancorp.net>
2020-02-21 10:25:16 +0530

    new code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list/events-list.component';
+import { EventsThumbnailComponent } from './events/events-thumbnail/events-thumbnail.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ EventsAppComponent],
+  declarations: [ EventsAppComponent, EventsListComponent, EventsThumbnailComponent, NavBarComponent],
   bootstrap:    [ EventsAppComponent ]
 })
 export class AppModule { }
---
+++ src/app/events/events-thumbnail/events-thumbnail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'events-thumbnail',
+  templateUrl: './events-thumbnail.component.html',
+  styleUrls: ['./events-thumbnail.component.css']
+})
+export class EventsThumbnailComponent implements OnInit {
+
+  @Input("eventVar") event:any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  colorTime (){
+    if(this.event && this.event.time === '8:00 am'){
+      return 'green bold' 
+    } else if(this.event && this.event.time === '10:00 am') {
+      return 'red bold';
+    }
+    return '';
+    }
+}
