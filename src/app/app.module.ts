37f0713ba5b897ef659b9a8fad605dd62df2610f
Akash Tripathi <tripk@ap.leaseplancorp.net>
2020-02-25 11:02:07 +0530

    added devent details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { EventsListComponent } from './events/events-list/events-list.component'
 import { EventsThumbnailComponent } from './events/events-thumbnail/events-thumbnail.component';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ EventsAppComponent, EventsListComponent, EventsThumbnailComponent, NavBarComponent],
+  declarations: [ EventsAppComponent, EventsListComponent, EventsThumbnailComponent, NavBarComponent, EventDetailsComponent],
   bootstrap:    [ EventsAppComponent ],
   providers: [EventService, ToastrService]
 })
